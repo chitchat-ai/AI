12b42892a5d8326f2b8fc83a8db6f3111148db29
Nazareka <nazar26032002@gmail.com>
2023-09-30 23:41:43 +0200

    rename DATABASE_URL

--- src/database.py
+++ src/database.py
@@ -4,6 +4,6 @@ from settings import settings
 
 
 async def get_database():
-    client = AsyncIOMotorClient(settings.MONGO_DB_URL)
+    client = AsyncIOMotorClient(settings.DATABASE_URL)
     return client.mydatabase  # replace `mydatabase` with your database name
 
